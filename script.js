58f02389c8ddcc4b25b1fdec87c1ef332ec0d1ec
HackYourFuture Belgium <turnamnehri17@outlook.com>
2019-02-17 12:17:17 +0100

    hh

---
+++ script.js
@@ -0,0 +1,9 @@
+document.write("<h2 style='color: blue'>javascript rules!</h2>");
+var age ="24";
+var phrase ="to be or not to be";
+document.write(phrase);
+phrase ="banana";
+document.write(phrase);
+phrase ="apple";
+document.write(phrase);
+var
